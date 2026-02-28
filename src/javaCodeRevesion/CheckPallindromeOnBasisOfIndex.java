1e379b27d0110ea353310d0995243d9105a81ff1
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2026-02-28 12:22:42 +0530

    Optimised code

--- src/javaCodeRevesion/CheckPallindromeOnBasisOfIndex.java
+++ src/javaCodeRevesion/CheckPallindromeOnBasisOfIndex.java
@@ -9,7 +9,7 @@ public class CheckPallindromeOnBasisOfIndex {
 	public static void checkPallindrome(String res) {
 
 		String rev = "";
-		for (int i = res.length()-1; i >=0; i--) {
+		for (int i = res.length() - 1; i >= 0; i--) {
 			char ch = res.charAt(i);
 			rev = rev + ch;
 		}
@@ -19,31 +19,23 @@ public class CheckPallindromeOnBasisOfIndex {
 		}
 	}
 
-	public static void checkInput(String s) { // madamcicictrhioinjkdfdjknkj
-		
-		for (int i = 0; i <= s.length()-5; i++) {
-			String inp = "";
-			for (int j = i; j < i+5; j++) {
-				char ch = s.charAt(j);
-				inp = inp + ch;
+	public static void checkInput(String s) {
+
+		int[] index = { 5, 3 };
+
+		for (int val : index) {
+
+			for (int i = 0; i <= s.length() - val; i++) {
+				String inp = "";
+				for (int j = i; j < i + val; j++) {
+					char ch = s.charAt(j);
+					inp = inp + ch;
+				}
+
+				checkPallindrome(inp);
 			}
-			
-			checkPallindrome(inp);
+
 		}
-
-
-		for (int i = 0; i <= s.length()-3; i++) {
-			String inp = "";
-			for (int j = i; j < i+3; j++) {
-				char ch = s.charAt(j);
-				inp = inp + ch;
-			}
-			checkPallindrome(inp);
-		}
-		
-
 	}
-	
-	
 
 }
