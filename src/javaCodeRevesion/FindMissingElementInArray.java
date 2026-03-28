d1f55fe2937af7bf32e753da38584b9149dc2c0b
pritip4 <ara.priti02@gmail.com>
2026-03-29 01:23:36 +0530

    added missing element

--- src/javaCodeRevesion/FindMissingElementInArray.java
+++ src/javaCodeRevesion/FindMissingElementInArray.java
@@ -4,22 +4,23 @@ import java.util.Arrays;
 
 public class FindMissingElementInArray {
 	public static void main(String[] args) {
-		int[] arr = {6,1,3,7,64};
-		
-	}
-	
-	
-	
-	public static void findMissingElement(int[] arr) {
-		Arrays.sort(arr);
-		for(int i=0;i<arr.length;i++) {
-			if((arr[i])+1 == arr[i+1]) {
-				break;
-			}else {
-				arr[i]=arr[i]+1;
-			}
-		}
-		
+		int[] arr = { 4, 6, 1, 3, 2, 9 };
+		findMissingElement(arr);
+
 	}
 
+	public static void findMissingElement(int[] arr) {
+
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length - 1; i++) {
+			int current = arr[i];
+
+			while (current + 1 < arr[i + 1]) {
+				current++;
+				System.out.println("Missing: " + current);
+			}
+
+		}
+
+	}
 }
