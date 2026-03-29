a1696b2e4170d213ba8a4d06a0b23eda7dcd1679
pritip4 <ara.priti02@gmail.com>
2026-03-29 09:18:35 +0530

    Added code to find largest element

--- src/javaCodeRevesion/LargestElementInArray.java
+++ src/javaCodeRevesion/LargestElementInArray.java
@@ -3,11 +3,22 @@ package javaCodeRevesion;
 public class LargestElementInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr= {4,1,9,6,5,12};
+		findLargestElementInArray(arr);
 
 	}
 	
-	public static void findLargestElementInArray() {
+	public static void findLargestElementInArray(int[] arr) {
+		int largest = arr[0];
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			if(largest<arr[i]) {
+				largest=arr[i];
+			}
+			
+		}
+		System.out.println(largest);
 		
 	}
 
