ad201f6e1bcc00680382766bc9e85e336a37de32
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2026-03-30 10:27:05 +0530

    Added code to remove Duplicate

--- src/javaCodeRevesion/RemoveDuplicateCharacterInString.java
+++ src/javaCodeRevesion/RemoveDuplicateCharacterInString.java
@@ -6,21 +6,21 @@ public class RemoveDuplicateCharacterInString {
 		removeDuplicateCharacter("shubham");
 
 	}
-	
+
 	public static void removeDuplicateCharacter(String input) {
 		char[] ch = input.toCharArray();
-		for(int i=0;i<input.length()-1;i++) {
-			boolean isDuplicate=false;
-			for(int j=i+1;j<input.length()-1;j++) {
-				if(ch[i]==ch[j]) {
-					isDuplicate=true;
+		for (int i = 0; i < input.length() - 1; i++) {
+			boolean isDuplicate = false;
+			for (int j = i + 1; j < input.length() - 1; j++) {
+				if (ch[i] == ch[j]) {
+					isDuplicate = true;
 					break;
-				}else {
-					System.out.print(ch[i]);
 				}
+			}
+			if (!isDuplicate) {
+				System.out.println(ch[i]);
 			}
 		}
 	}
 
-	
 }
