8b90819a2419b2fbbe65281223c3471deb2cf69e
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2026-04-02 09:23:49 +0530

    removed pom.xml file from git ignore

--- src/javaCodeRevesion/CommonPrefix.java
+++ src/javaCodeRevesion/CommonPrefix.java
@@ -1,42 +1,60 @@
 package javaCodeRevesion;
 
+import java.util.Arrays;
+
 public class CommonPrefix {
 
 	public static void main(String[] args) {
-		String[] arr = { "fly", "flight", "f" };
-		String res = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			res = commonPrefix(res, arr[i]);
-
-		}
-		System.out.println("Res2" + res);
-
-	}
-
-	public static String commonPrefix(String s1, String s2) {
-		int c1 = s1.length();
-		int c2 = s2.length();
+		
+		String[] arr = {"flower", "flyw","flow","flight","f"};
+		
+		char[] ch1 = arr[0].toCharArray();
+		//System.out.println(ch1.length);
+		char[] ch2 = arr[1].toCharArray();
+	//	System.out.println(ch2.length);
 		String res = "";
-
-		int iterateTime;
-
-		if (c1 > c2) {
-			iterateTime = c2;
-		} else {
-			iterateTime = c1;
+		int iteratiTime = 0;
+		
+		
+		if(ch1.length<ch2.length) {
+			iteratiTime=ch1.length;
+		}else {
+			iteratiTime=ch2.length;
 		}
-
-		for (int i = 0; i < iterateTime; i++) {
-			char ch1 = s1.charAt(i);
-			char ch2 = s2.charAt(i);
-			if (ch1 == ch2) {
-				res = res + ch1;
-			} else {
+		
+	//	System.out.println(iteratiTime);
+		for(int i=0;i<iteratiTime;i++) {
+			if(ch1[i]==ch2[i]) {
+				res=res+ch1[i];
+			}else {
 				break;
 			}
-
 		}
-		return res;
+		
+		for(int k=2;k<arr.length;k++) {
+			String finalResult= "";
+			
+			char[] res1= res.toCharArray();
+			System.out.println(res1[0]);
+			char[] ch3 = arr[k].toCharArray();
+			
+			
+			int len = Math.min(res1.length, ch3.length); 
+			
+			for(int j=0;j<len;j++) {
+				
+				if(res1[j]==ch3[j]) {
+					finalResult = finalResult+ch3[j];
+				}else {
+					break;
+				}
+			}
+			
+			res=finalResult;
+		}
+		System.out.println(res);
+		
+
 	}
 
 }
---
+++ src/javaCodeRevesion/codeRevision.java
@@ -0,0 +1,17 @@
+package javaCodeRevesion;
+
+import java.util.Arrays;
+
+public class codeRevision {
+	
+	//remove duplicate character 
+
+	public static void main(String[] args) {
+		
+		String s = "shashank";
+		for(int i=0;i<s.length();i++) {
+			
+		}
+	
+}
+}
